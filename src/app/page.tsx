ad0d5074b1bd0a79e557ba64613e3d45db44a295
noahvanpuymbroeck <noahvp@icloud.com>
2025-03-09 10:41:18 +0100

    removed graphic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import BelgiumMapWithGraph from "../../components/BelgiumMap";
-import DistanceChart from "../../components/DistanceChart";
 
 export default function Home() {
-    return (
-        <main style={{ display: "flex", justifyContent: "center", gap: "20px", alignItems: "start" }}>
-            <div style={{ flex: 1 }}>
-                <h1>Belgium Map with D3.js</h1>
-                <BelgiumMapWithGraph />
-            </div>
-            <div style={{ flex: 1 }}>
-                <DistanceChart />
-            </div>
-        </main>
-    );
+  return (
+      <main>
+        <h1>Belgium Map with D3.js</h1>
+        <BelgiumMapWithGraph />
+      </main>
+  );
 }
-
